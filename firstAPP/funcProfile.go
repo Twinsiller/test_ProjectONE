60197b5c608ec022179bb2a3d5d2b20b3ac65eaf
Twinsiller <boldinov.leha@yandex.ru>
2024-11-15 18:06:31 +0300

    connectDB

--- firstAPP/funcProfile.go
+++ firstAPP/funcProfile.go
@@ -1,97 +1,101 @@
 package main
 
-import (
-	"net/http"
-	"strconv"
+// import (
+// 	"net/http"
+// 	"strconv"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-type Profile struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
-}
+// type Profile struct {
+// 	id           int    `json:"id"`
+// 	nickname     int    `json:"nickname"`
+// 	hashPassword string `json:"hashPassword"`
+// 	status       string `json:"status"`
+// 	accessLevel  string `json:"accessLevel"`
+// 	name         string `json:"name"`
+// 	surname      int    `json:"surname"`
+// }
 
-var countIdProfiles int = 2
-var profiles = []Profile{
-	{ID: 0, Name: "Alex", Password: "322"},
-	{ID: 1, Name: "Valerchik", Password: "52"},
-	{ID: 2, Name: "Igar", Password: "894"},
-}
+// var countidProfiles int = 2
+// var profiles = []Profile{
+// 	{id: 0, name: "Alex", hashPassword: "322"},
+// 	{id: 1, name: "Valerchik", hashPassword: "52"},
+// 	{id: 2, name: "Igar", hashPassword: "894"},
+// }
 
-func GetProfiles(c *gin.Context) {
-	c.JSON(http.StatusOK, profiles)
-}
+// func GetProfiles(c *gin.Context) {
+// 	c.JSON(http.StatusOK, profiles)
+// }
 
-func GetProfileByID(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
-		return
-	}
+// func GetProfileByid(c *gin.Context) {
+// 	idStr := c.Param("id")
+// 	id, err := strconv.Atoi(idStr)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
+// 		return
+// 	}
 
-	for _, profile := range profiles {
-		if profile.ID == id {
-			c.JSON(http.StatusOK, profile)
-			return
-		}
-	}
-	c.JSON(http.StatusNotFound, gin.H{"message": "profile not found"})
-}
+// 	for _, profile := range profiles {
+// 		if profile.id == id {
+// 			c.JSON(http.StatusOK, profile)
+// 			return
+// 		}
+// 	}
+// 	c.JSON(http.StatusNotFound, gin.H{"message": "profile not found"})
+// }
 
-func CreateProfile(c *gin.Context) {
-	var newProfile Profile
+// func CreateProfile(c *gin.Context) {
+// 	var newProfile Profile
 
-	if err := c.BindJSON(&newProfile); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
-		return
-	}
-	countIdProfiles++
-	newProfile.ID = countIdProfiles
-	profiles = append(profiles, newProfile)
-	c.JSON(http.StatusCreated, newProfile)
-}
+// 	if err := c.BindJSON(&newProfile); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+// 		return
+// 	}
+// 	countidProfiles++
+// 	newProfile.id = countidProfiles
+// 	profiles = append(profiles, newProfile)
+// 	c.JSON(http.StatusCreated, newProfile)
+// }
 
-func UpdateProfile(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
-		return
-	}
-	var updatedProfile Profile
+// func UpdateProfile(c *gin.Context) {
+// 	idStr := c.Param("id")
+// 	id, err := strconv.Atoi(idStr)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
+// 		return
+// 	}
+// 	var updatedProfile Profile
 
-	if err := c.BindJSON(&updatedProfile); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
-		return
-	}
+// 	if err := c.BindJSON(&updatedProfile); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+// 		return
+// 	}
 
-	for i, profile := range profiles {
-		if profile.ID == id {
-			profiles[i] = updatedProfile
-			c.JSON(http.StatusOK, updatedProfile)
-			return
-		}
-	}
+// 	for i, profile := range profiles {
+// 		if profile.id == id {
+// 			profiles[i] = updatedProfile
+// 			c.JSON(http.StatusOK, updatedProfile)
+// 			return
+// 		}
+// 	}
 
-	c.JSON(http.StatusNotFound, gin.H{"message": "book not found"})
-}
+// 	c.JSON(http.StatusNotFound, gin.H{"message": "book not found"})
+// }
 
-func DeleteProfile(c *gin.Context) {
-	idStr := c.Param("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
-		return
-	}
-	for i, profile := range profiles {
-		if profile.ID == id {
-			profiles = append(profiles[:i], profiles[i+1:]...)
-			c.JSON(http.StatusOK, gin.H{"message": "profile was deleted"})
-			return
-		}
-	}
-	c.JSON(http.StatusNotFound, gin.H{"message": "profile not found for delete"})
-}
+// func DeleteProfile(c *gin.Context) {
+// 	idStr := c.Param("id")
+// 	id, err := strconv.Atoi(idStr)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid id"})
+// 		return
+// 	}
+// 	for i, profile := range profiles {
+// 		if profile.id == id {
+// 			profiles = append(profiles[:i], profiles[i+1:]...)
+// 			c.JSON(http.StatusOK, gin.H{"message": "profile was deleted"})
+// 			return
+// 		}
+// 	}
+// 	c.JSON(http.StatusNotFound, gin.H{"message": "profile not found for delete"})
+// }
--- firstAPP/main.go
+++ firstAPP/main.go
@@ -1,36 +1,96 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+	password "github.com/vzglad-smerti/password_hash"
 )
 
+//	func main() {
+//		router := gin.Default()
+//		// Получение всех профилей
+//		router.GET("/profiles", GetProfiles)
+//		// Получение всех постов
+//		router.GET("/posts", GetPosts)
+//		// Получение поста по ID
+//		router.GET("/profiles/:id", GetProfileByID)
+//		// Получение профиля по ID
+//		router.GET("/posts/:id", GetPostByID)
+//		// Создание нового профиля
+//		router.POST("/profiles", CreateProfile)
+//		// Создание новой поста
+//		router.POST("/posts", CreatePost)
+//		// Обновление существующего профиля
+//		router.PUT("/profiles/:id", UpdateProfile)
+//		// Обновление существующего поста
+//		router.PUT("/posts/:id", UpdatePost)
+//		// Удаление профиля
+//		router.DELETE("/profiles/:id", DeleteProfile)
+//		// Удаление поста
+//		router.DELETE("/posts/:id", DeletePost)
+//		router.Run(":8080")
+//	}
+type Profile struct {
+	id           int       `json:"id"`
+	nickname     string    `json:"nickname"`
+	hashPassword string    `json:"hashPassword"`
+	status       bool      `json:"status"`
+	accessLevel  int       `json:"accessLevel"`
+	name         string    `json:"name"`
+	lastname     string    `json:"lastname"`
+	createdAT    time.Time `json:"createdAT"`
+}
+
 func main() {
-	router := gin.Default()
+	connStr := "user=postgres password=1706 dbname=ProjectONE sslmode=disable host=localhost port=5432"
 
-	// Получение всех профилей
-	router.GET("/profiles", GetProfiles)
-	// Получение всех постов
-	router.GET("/posts", GetPosts)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Done!")
+	defer db.Close()
 
-	// Получение поста по ID
-	router.GET("/profiles/:id", GetProfileByID)
-	// Получение профиля по ID
-	router.GET("/posts/:id", GetPostByID)
+	//пароль который будем проверять
+	password_users := "testing"
+	//создадим хеш пароля
+	hash, err := password.Hash(password_users)
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(hash)
 
-	// Создание нового профиля
-	router.POST("/profiles", CreateProfile)
-	// Создание новой поста
-	router.POST("/posts", CreatePost)
+	result, err := db.Exec("insert into profiles (nickname, hash_password, status, access_level, firstname, lastname, created_at) values ( $1, $2, $3, $4, $5, $6, $7)",
+		"Twinsiller", hash, true, 5, "Alex", "Boldinov", time.Now())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.LastInsertId()) // не поддерживается
+	fmt.Println(result.RowsAffected()) // количество добавленных строк
 
-	// Обновление существующего профиля
-	router.PUT("/profiles/:id", UpdateProfile)
-	// Обновление существующего поста
-	router.PUT("/posts/:id", UpdatePost)
+	rows, err := db.Query("select * from profiles")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	profiles := []Profile{}
 
-	// Удаление профиля
-	router.DELETE("/profiles/:id", DeleteProfile)
-	// Удаление поста
-	router.DELETE("/posts/:id", DeletePost)
-
-	router.Run(":8080")
+	for rows.Next() {
+		p := Profile{}
+		err := rows.Scan(&p.id, &p.nickname, &p.hashPassword, &p.status, &p.accessLevel, &p.name, &p.lastname, &p.createdAT)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		profiles = append(profiles, p)
+	}
+	for i, p := range profiles {
+		fmt.Println(i, "dsadasdasnlda")
+		fmt.Println(p.id, p.nickname, p.status, p.accessLevel, p.name, p.lastname)
+	}
+	fmt.Println("Exit")
 }
