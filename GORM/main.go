429113d20cb4e839dd8858c2130ad022c46a24e6
Twinsiller <boldinov.leha@yandex.ru>
2024-12-28 14:23:23 +0300

    ш

--- GORM/main.go
+++ GORM/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,6 +14,11 @@ import (
 var DbPostgres *gorm.DB
 
 func initDB() {
+	// Загружаем переменные окружения из файла .env
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Error loading .env file")
+		return
+	}
 	// Config — структура для хранения конфигурации подключения
 	cfg := struct {
 		User     string
@@ -28,10 +35,10 @@ func initDB() {
 		DBName:   os.Getenv("DB_NAME"),
 		SSLMode:  os.Getenv("DB_SSLMODE"),
 	}
-	fmt.Println("sadas ", cfg.User)
+	//res, ok := os.LookupEnv("DB_PASSWORD")
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		cfg.Host, os.Getenv("DB_USER"), cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode,
+		cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode,
 	)
 	fmt.Println(dsn)
 	var err error
@@ -42,6 +49,22 @@ func initDB() {
 	fmt.Println("DB is working")
 }
 
+type Profile struct {
+	Id           int       `json:"id",gorm:"primaryKey;"`
+	Nickname     string    `json:"nickname",gorm:""`
+	HashPassword string    `json:"hashpassword",gorm:""`
+	Status       bool      `json:"status",gorm:""`
+	AccessLevel  int       `json:"accesslevel",gorm:""`
+	Firstname    string    `json:"firstname",gorm:""`
+	Lastname     string    `json:"lastname",gorm:""`
+	CreatedAt    time.Time `json:"createdat",gorm:""`
+}
+
+func createTables() {
+	DbPostgres.AutoMigrate(&Profile{})
+}
+
 func main() {
 	initDB()
+	createTables()
 }
